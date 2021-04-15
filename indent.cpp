0a15dd46614cebe635b5f66c9a3abf1c327826da
William Anderson <48596907+William-Anderson2020@users.noreply.github.com>
2021-04-15 18:38:47 -0400

    Fix misuse of str.find

--- indent.cpp
+++ indent.cpp
@@ -38,7 +38,7 @@ int indent_code(std::string filename) {
             std::cout << "LOOP CLOSED" << std::endl;
         }
 
-        if((line.find("if") || line.find("for")) && line[-1] != '{'){ //Checks line for "if" or "for". If found and final character isn't {, add new indent to accomodate for single line statement.
+        if((line.find("if") == 0 || line.find("for") == 0) && line[-1] != '{'){ //Checks line for "if" or "for". If found and final character isn't {, add new indent to accomodate for single line statement.
             tabs += 1;
             singleLineLoop = true; //Marks that a single line loop was just indented
             std::cout << "LOOP OPENED" << std::endl;
@@ -50,7 +50,7 @@ int indent_code(std::string filename) {
 
     }
 
-    std::string test = "test if detection";
+    std::string test = "if test if detection";
     std::cout << test.find("for") << std::endl;
 
     return 0;
